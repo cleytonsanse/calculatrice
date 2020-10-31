f84f808b8966a52c7e4082fc2558043ed0bc86be
Cleyton SanSe <cleyton@fafofri.com>
2020-11-01 00:30:09 +0100

    PYTHON-3 implementer addition

---
+++ operation/addition.py
@@ -0,0 +1,19 @@
+# déclarer une variable terme1 qui a pour valeur 42
+# terme1=42 # terme1 est une variable
+
+# déclarer une variable terme2 qui a pour valeur 21
+# terme2=21 # terme2 est une variable
+
+# déclarer une variable somme qui a pour valeur l'addition de terme1 et terme2 (terme1+terme2)
+# somme=terme1+terme2 # somme est une variable
+
+# afficher la variable somme
+#print(somme) # print() est une fonction qui permet d'afficher la valeur d'une variable
+
+# définir la fonction "additioner" qui prend comme paramètres a et b
+# et qui affiche le résultat de l'addition de a et b
+def additioner(a, b):
+    print(a+b)
+
+# faire appel à la fonction "additionner" avec comme paramètre terme1 et terme2
+#additioner(terme1, terme2)
