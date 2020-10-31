490678bd9a2f0bf33afe0851c69cb59be6f1e4d4
Cleyton SanSe <cleyton@fafofri.com>
2020-11-01 00:44:40 +0100

    PYTHON-4 implementer soustraction

---
+++ operation/soustraction.py
@@ -0,0 +1,19 @@
+# déclarer une variable a qui a pour valeur 42
+# terme1=42 # terme1 est une variable
+
+# déclarer une variable b qui a pour valeur 21
+# terme2=21 # terme2 est une variable
+
+# déclarer une variable difference qui a pour valeur la soustraction de terme1 et terme2 (terme1-terme2)
+# difference=terme1-terme2 # difference est une variable
+
+# afficher la variable difference
+# print(difference) # print() est une fonction
+
+# définir la fonction "soustraire" qui prend comme paramètres a et b
+# et qui affiche le résultat de la soustraction de a et b
+def soustraire(a, b):
+    print(a-b)
+
+# faire appel à la fonction "soustraire" avec comme paramètre terme1 et terme2
+# soustraire(terme1, terme2)
