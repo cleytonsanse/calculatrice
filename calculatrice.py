7950fa3d440cfe2d0d65e51c35b101d5fa37c32a
Cleyton SanSe <cleyton@fafofri.com>
2020-11-01 00:57:23 +0100

    PYTHON-2 implementer les operations basiques

---
+++ calculatrice.py
@@ -0,0 +1,9 @@
+from .operation.addition import additioner
+from .operation.soustraction import soustraire
+from .operation.multiplication import multiplier
+from .operation.division import diviser
+
+additioner(1, 2)
+soustraire(16, 5)
+multiplier(5, 5)
+diviser(45, 15)
