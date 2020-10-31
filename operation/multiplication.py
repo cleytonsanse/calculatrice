2d7a7a7724a93a7f1f2a6096ee529edea11d4998
Cleyton SanSe <cleyton@fafofri.com>
2020-11-01 00:48:34 +0100

    PYTHON-5 implementer multiplication

---
+++ operation/multiplication.py
@@ -0,0 +1,19 @@
+# déclarer une variable multiplicande qui a pour valeur 42
+# multiplicande=42 # multiplicande est une variable
+
+# déclarer une variable multiplicateur qui a pour valeur 21
+# multiplicateur=21 # multiplicateur est une variable
+
+# déclarer une variable produit qui a pour valeur la multiplication de multiplicande par multiplicateur (multiplicande*multiplicateur)
+# produit=multiplicande*multiplicateur # produit est un variable
+
+# afficher la variable produit
+# print(produit) # print() est une fonction
+
+# définir la fonction "multiplier" qui prend comme paramètres a et b
+# et qui affiche le résultat de la multiplication de a par b
+def multiplier(a, b):
+    print(a*b)
+
+# faire appel à la fonction "multiplier" avec comme paramètre multiplicande et multiplicateur
+# multiplier(multiplicande, multiplicateur)
