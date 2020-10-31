cbc1549571486990407ada0c5fbea3f4361e0d1b
Cleyton SanSe <cleyton@fafofri.com>
2020-11-01 00:49:36 +0100

    PYTHON-6 implementer division

---
+++ operation/division.py
@@ -0,0 +1,19 @@
+# déclarer une variable dividende qui a pour valeur 30
+# dividende=30 # dividende est une variable
+
+# déclarer une variable diviseur qui a pour valeur 20
+# diviseur=20 # diviseur est une variable
+
+# déclarer une variable quotient qui a pour valeur la division de dividende par diviseur (dividende/diviseur)
+# quotient=dividende/diviseur # quotient est une variable
+
+# afficher la variable quotient
+# print(quotient) # print() est une fonction
+
+# définir la fonction "diviser" qui prend comme paramètres a et b
+# et qui affiche le résultat de la division de a par b
+def diviser(a, b):
+    print(a/b)   
+
+# faire appel à la fonction "diviser" avec comme paramètre dividende et diviseur
+# diviser(dividende, diviseur)
